74be811492af9ccaa5368f1332a829a0fd07b2fd
Dario Malchiodi <malchiodi@malchiodi.docenti.di.unimi.it>
2025-01-31 15:54:38 +0100

    version 0.5.4b

--- learnedbf/__init__.py
+++ learnedbf/__init__.py
@@ -18,7 +18,7 @@ from learnedbf.classifiers import ScoredDecisionTreeClassifier
 # TODO: check what happens with the `classes_` attribute of classifiers
 #       not based on trees
 
-__version__ = 0.5.4a
+__version__ = 0.5.4b
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
